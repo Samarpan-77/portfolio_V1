b6ff35a6fa6ccf2c7170995e4b74fdba2301bd8c
Sam-test-007 <Summer_pan77@proton.me>
2026-03-16 21:05:14 +0545

    push

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: 'https://github.com/Samarpan-77/portfolio_V1.git',
+    base: '/portfolio_V1',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
